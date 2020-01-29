3a0f4cc03bb63a9b9a0f61439e1a132ab72b2390
Lee McCormick <lzm0010@gmail.com>
2020-01-29 20:07:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+  
+  
+end
