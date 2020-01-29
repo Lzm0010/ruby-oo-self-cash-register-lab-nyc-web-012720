0f70e22f7aa85b18e3a03099f50c56bd44f353ca
Lee McCormick <lzm0010@gmail.com>
2020-01-29 20:18:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   
   def apply_discount
     self.total *= (1 - discount)
-    puts "Success"
+    puts "After the discount, the total comes to $#{self.discount}"
   end
   
   def items
